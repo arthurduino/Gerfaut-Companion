498feb6ceed9175852cd0e5f817a4f24cf7b946c
Arthur Gerfaut <arthur@gerfaut.mooo.com>
2026-02-03 01:19:56 +0000

    Release v1.1.7 - Retrait du filtrage du contenu additionnel des emails

--- includes/class-woo-email-savelink.php
+++ includes/class-woo-email-savelink.php
@@ -12,27 +12,6 @@ class Gerfaut_Companion_Woo_Email_SavLink
     public function __construct()
     {
         add_action('woocommerce_email_after_order_table', array($this, 'render_sav_link'), 20, 4);
-        add_filter('woocommerce_email_additional_content_new_order', array($this, 'filter_additional_content'), 10, 3);
-        add_filter('woocommerce_email_additional_content_customer_processing_order', array($this, 'filter_additional_content'), 10, 3);
-        add_filter('woocommerce_email_additional_content_customer_completed_order', array($this, 'filter_additional_content'), 10, 3);
-    }
-
-    /**
-     * Filtre le contenu additionnel pour supprimer les mentions de contact par mail
-     */
-    public function filter_additional_content($additional_content, $order, $email)
-    {
-        // Supprimer les phrases qui mentionnent de contacter par mail/email
-        $patterns = array(
-            '/contactez[\s-]nous\s+à\s+l[\'']adresse\s+[^\s]+\s+si\s+vous\s+avez\s+besoin\s+d[\'']aide[^.!?]*(\.|\!|\?)/i',
-            '/si\s+vous\s+avez\s+besoin\s+d[\'']aide.*contactez[\s-]nous[^.!?]*(\.|\!|\?)/i',
-        );
-        
-        foreach ($patterns as $pattern) {
-            $additional_content = preg_replace($pattern, '', $additional_content);
-        }
-        
-        return trim($additional_content);
     }
 
     public function render_sav_link($order, $sent_to_admin, $plain_text, $email)
